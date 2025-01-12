dcf829377bd9121350f0929b8d6d61cbf289870a
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-01-12 07:49:39 +0530

    update file

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-let div = document.querySelector("div");
-let check = confirm("Do you love me?");
+let startBtn = document.querySelector(".start");
+let yesBtn = document.querySelector("#yesBtn");
+let noBtn = document.querySelector("#noBtn");
+let para = document.querySelector("p");
+let div = document.querySelector(".modal-content");
 
-while (true) {
-  if (check == true) {
-    div.innerText = "I Love you too❤️";
-    break;
-  } else {
-    check = confirm("Thing again😡😡😡. Do you love me?");
-  }
+startBtn.addEventListener("click", function () {
+  this.style.display = "none";
+  div.style.display = "block";
+});
+
+noBtn.addEventListener("click", clickkaro2);
+yesBtn.addEventListener("click", clickkaro);
+
+function clickkaro() {
+  para.innerText = "I Love you too❤️";
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+}
+
+function clickkaro2() {
+  alert("Think again, If you want to live 😡😡😡");
+  para.innerText = "😡 Do you Love me?";
 }
